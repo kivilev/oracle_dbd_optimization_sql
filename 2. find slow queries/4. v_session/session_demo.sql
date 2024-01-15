baa114cb572c0bd32e83856b454128cb3d419293
Denis Kivilev <kivilev.d@gmail.com>
2024-01-15 15:19:37 +0300

    Add multisession examples

--- 2. find slow queries/4. v_session/session_demo.sql
+++ 2. find slow queries/4. v_session/session_demo.sql
@@ -1,13 +1,15 @@
-п»ї---- Р”РµРјРѕ РїСЂРёРјРёС‚РёРІРЅРѕР№ РґРёР°РіРЅРѕСЃС‚РёРєРё РїРѕ v$session (HR)
+---- Демо примитивной диагностики по v$session (HR)
 
--- 1) РѕС‚РєСЂС‹РІР°РµРј РґСЂСѓРіСѓСЋ РІРєР»Р°РґРєСѓ СЃ СЃРµСЃСЃРёСЏРјРё
+---- Пример 1. Одна сесссия
 
--- 2) РІ СЌС‚РѕР№ РІРєР»Р°РґРєРµ Р·Р°РїСѓСЃРєР°Рј
+-- 1) открываем другую вкладку с сессиями
+
+-- 2) в этой вкладке запускам
 
 declare
   v_cnt number;
 begin
-  dbms_session.sleep(5); -- С‡С‚РѕР± СѓСЃРїРµР» РїРµСЂРµРєР»СЋС‡РёС‚СЊСЃСЏ РІРѕ РІРєР»Р°РґРєСѓ СЃ СЃРµСЃСЃРёСЏРјРё
+  dbms_session.sleep(5); -- чтоб успел переключиться во вкладку с сессиями
 
   select
    count(*)
@@ -35,3 +37,85 @@ begin
 
 end;
 /
+
+
+---- Пример 2. Имитация нескольких сессий
+
+-- создаем процедурку с тормозами
+create or replace procedure del$client_activity
+is
+  v_cnt number;
+begin
+  select count(*) into v_cnt from dual;
+  
+  dbms_application_info.set_action(action_name => 'Q1 running...');
+  
+  select
+   count(*)
+    into v_cnt
+    from hr.employees q1
+   cross join hr.employees
+   cross join hr.employees
+   cross join hr.employees;
+
+
+  dbms_application_info.set_action(action_name => 'Q2 running...');
+
+  select
+   count(*)
+    into v_cnt
+    from hr.employees q2
+   cross join hr.employees
+   cross join hr.employees
+   cross join hr.employees;
+
+  dbms_application_info.set_action(action_name => 'Q3 running...');
+
+  select 
+   count(*)
+    into v_cnt
+    from hr.employees q3
+   cross join hr.employees;
+
+  dbms_application_info.set_action(action_name => 'Q4 running...');
+
+  select 
+   count(*)
+    into v_cnt
+    from hr.employees q4
+   cross join hr.employees
+   cross join hr.employees;
+
+end;
+/
+
+-- создаем 10 сессий
+declare
+  v_session_count number := 10;
+begin
+  for i in 1..v_session_count loop
+  dbms_scheduler.create_job(job_name        => 'demo_v_session_job_'||i,
+                              job_type        => 'STORED_PROCEDURE',
+                              job_action      => 'del$client_activity',
+                              start_date      => systimestamp,
+                              repeat_interval => 'freq=SECONDLY;',
+                              end_date        => null,
+                              enabled         => true,
+                              auto_drop       => false,
+                              comments        => 'Some client activity');
+  end loop;                            
+end;
+/
+
+select sysdate, t.next_run_date, t.* from user_scheduler_jobs t where lower(t.job_name) like 'demo_v_session_job_%';
+
+-- чистим
+declare
+  v_session_count number := 10;
+begin
+  for i in 1..v_session_count loop
+    dbms_scheduler.drop_job(job_name => 'demo_v_session_job_'||i, force => true); 
+  end loop;                            
+end;
+/
+
