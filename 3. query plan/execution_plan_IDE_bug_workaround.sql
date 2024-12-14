1022f24dfb06d7ded676986d35b1c7fbcde8bfab
Denis Kivilev <kivilev.d@gmail.com>
2024-12-14 08:32:09 +0300

    Clear format mask, add versions

--- 3. query plan/execution_plan_IDE_bug_workaround.sql
+++ 3. query plan/execution_plan_IDE_bug_workaround.sql
@@ -3,7 +3,7 @@ begin
   dbms_output.enable();
 
   execute immediate 'alter session set statistics_level = ALL';
-
+  --execute immediate q'{alter session set optimizer_features_enable  = '10.1.0'}';
 
   for test in (
 
@@ -17,9 +17,17 @@ begin
     null;
   end loop;
 
-  for x in (select p.plan_table_output from table(dbms_xplan.display_cursor(null, null, 'ALLSTATS ADVANCED LAST ADAPTIVE')) p) loop
+  for x in (select p.plan_table_output from table(dbms_xplan.display_cursor(null, null, 'ALLSTATS ADVANCED LAST -cost -bytes')) p) loop
     dbms_output.put_line(x.plan_table_output);
   end loop;
   rollback;
 end;
 /
+
+
+/*
+-- possible versions
+19.1.0.1, 19.1.0, 18.1.0, 12.2.0.1, 12.1.0.2, 12.1.0.1, 11.2.0.4, 11.2.0.3, 11.2.0.2, 11.2.0.1, 11.1.0.7,
+11.1.0.6, 10.2.0.5, 10.2.0.4, 10.2.0.3, 10.2.0.2, 10.2.0.1, 10.1.0.5, 10.1.0.4, 10.1.0.3, 10.1.0,
+9.2.0.8, 9.2.0, 9.0.1, 9.0.0, 8.1.7, 8.1.6, 8.1.5, 8.1.4, 8.1.3, 8.1.0, 8.0.7, 8.0.6, 8.0.5, 8.0.4, 8.0.3, 8.0.0
+*/
